219e034585a24345af3a74181f2c7502001e0252
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-02 23:34:48 +0530

    setsup for navbar

--- qaa-question-everything-client/src/components/app.jsx
+++ qaa-question-everything-client/src/components/app.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from './navBar.jsx';
 
 class App extends Component {
   render(){
     return(
       <div>
+        <NavBar />
         App
       </div>
     );
