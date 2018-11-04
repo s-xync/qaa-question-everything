1d3371649c56748267be26c57cb30f5a3f84233b
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-04 20:07:58 +0530

    done with conditional rendering of navitems for logged in and not logged in

--- qaa-question-everything-client/src/components/navBar.jsx
+++ qaa-question-everything-client/src/components/navBar.jsx
@@ -1,9 +1,34 @@
 import React, { Component, Fragment } from 'react';
 import { NavbarBrand, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
 class NavBar extends Component {
-  
+
+  renderLoggedInNavItems = () => {
+    if(this.props.validSession){
+      return(
+        <Fragment>
+          <NavItem>
+            <NavLink tag={Link} to="/profile">{this.props.firstName + " " + this.props.lastName}</NavLink>
+          </NavItem>
+        </Fragment>
+      );
+    }else{
+      return(
+        <Fragment>
+          <NavItem>
+            <NavLink tag={Link} to="/signup">Signup</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/login">Login</NavLink>
+          </NavItem>
+        </Fragment>
+      );
+    }
+  }
+
   render(){
     return(
       <Fragment>
@@ -14,12 +39,7 @@ class NavBar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
-              <NavItem>
-                <NavLink tag={Link} to="/signup">Signup</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="/login">Login</NavLink>
-              </NavItem>
+              { this.renderLoggedInNavItems() }
             </ul>
           </div>
         </nav>
@@ -28,4 +48,16 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  validSession : propTypes.bool.isRequired,
+  firstName : propTypes.string,
+  lastName : propTypes.string
+};
+
+const mapStateToProps = ({ user }) => ({
+  validSession : user.validSession,
+  firstName : user.firstName,
+  lastName : user.lastName
+});
+
+export default connect(mapStateToProps, {  })(NavBar);
