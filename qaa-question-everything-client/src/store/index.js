c9c975530837da3f20dff3bb490d1258d1d9a56d
SaiKumar Immadi <immadisaikumar@ymail.com>
2018-11-06 02:02:00 +0530

    adds optional commented line for redux dev tools

--- qaa-question-everything-client/src/store/index.js
+++ qaa-question-everything-client/src/store/index.js
@@ -4,6 +4,14 @@ import rootReducer from '../reducers';
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+/*
+****
+If you have redux dev tool extension in your browser, then you can uncomment the below line and comment the other one.
+****
+*/
+
+// const store = createStore(rootReducer, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 export default store;
